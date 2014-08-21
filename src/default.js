2c7a2351d8b71e357849c43e1ff419936fcb43e6
Simon de Haan <simon@praekeltfoundation.org>
2014-08-21 07:22:51 +0200

    starting on delegate state

--- src/default.js
+++ src/default.js
@@ -5,6 +5,21 @@ go.app = function() {
 
     var GoMAMA = App.extend(function(self) {
         App.call(self, 'states_start');
+
+        self.states.add('states_start', function (name, opts) {
+            return self.im.api_request('optout.status', {
+                address_type: "msisdn",
+                address_value: self.im
+            }).then(function (result) {
+                if(result.opted_out) {
+                    return self.states.create('opted_out');
+                } else if (self.im.config.default_language) {
+                    return self.states.create('default_language');
+                } else {
+                    return self.states.create('language_selection');
+                }
+            });
+        });
     });
 
     return {
