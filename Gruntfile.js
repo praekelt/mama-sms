08e25567bb4cb97d00a54917ab6a9a84706d5017
Simon de Haan <simon@praekeltfoundation.org>
2014-08-21 07:27:37 +0200

    4 -> 2 spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,147 +1,147 @@
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.initConfig({
-        paths: {
-            src: {
-                'app': {
-                    'default': 'src/default.js',
-                    'rsa': 'src/default.js',
-                    'kenya': 'src/kenya.js',
-                    'utils': 'src/utils.js'
-                },
-                'default': [
-                    'src/index.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.default %>',
-                    'src/init.js'
-                ],
-                'rsa': [
-                    'src/index.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.default %>',
-                    'src/holodeck.js',
-                    'src/init.js'
-                ],
-                'kenya': [
-                    'src/index.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.kenya %>',
-                    'src/init.js'
-                ],
-                'utils': [
-                    'src/index.js',
-                    'src/constants.js',
-                    'src/utils.js'
-                ],
-                'all': [
-                    'src/**/*.js'
-                ]
-            },
-            dest: {
-                'default': 'go-app-default.js',
-                'rsa': 'go-app-rsa.js',
-                'kenya': 'go-app-kenya.js'
-            },
-            test: {
-                'default': [
-                    'test/setup.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.default %>',
-                    'test/default.test.js'
-                ],
-                'rsa': [
-                    'test/setup.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.default %>',
-                    'test/default.test.js'
-                ],
-                'kenya': [
-                    'test/setup.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.kenya %>',
-                    'test/kenya.test.js'
-                ],
-                'utils': [
-                    'test/setup.js',
-                    'src/constants.js',
-                    'src/utils.js',
-                    '<%= paths.src.app.utils %>',
-                    'test/utils.test.js'
-                ]
-            }
+  grunt.initConfig({
+    paths: {
+      src: {
+        'app': {
+          'default': 'src/default.js',
+          'rsa': 'src/default.js',
+          'kenya': 'src/kenya.js',
+          'utils': 'src/utils.js'
         },
+        'default': [
+          'src/index.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.default %>',
+          'src/init.js'
+        ],
+        'rsa': [
+          'src/index.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.default %>',
+          'src/holodeck.js',
+          'src/init.js'
+        ],
+        'kenya': [
+          'src/index.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.kenya %>',
+          'src/init.js'
+        ],
+        'utils': [
+          'src/index.js',
+          'src/constants.js',
+          'src/utils.js'
+        ],
+        'all': [
+          'src/**/*.js'
+        ]
+      },
+      dest: {
+        'default': 'go-app-default.js',
+        'rsa': 'go-app-rsa.js',
+        'kenya': 'go-app-kenya.js'
+      },
+      test: {
+        'default': [
+          'test/setup.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.default %>',
+          'test/default.test.js'
+        ],
+        'rsa': [
+          'test/setup.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.default %>',
+          'test/default.test.js'
+        ],
+        'kenya': [
+          'test/setup.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.kenya %>',
+          'test/kenya.test.js'
+        ],
+        'utils': [
+          'test/setup.js',
+          'src/constants.js',
+          'src/utils.js',
+          '<%= paths.src.app.utils %>',
+          'test/utils.test.js'
+        ]
+      }
+    },
 
-        jshint: {
-            options: {jshintrc: '.jshintrc'},
-            all: [
-                'Gruntfile.js',
-                '<%= paths.src.all %>'
-            ]
-        },
+    jshint: {
+      options: {jshintrc: '.jshintrc'},
+      all: [
+        'Gruntfile.js',
+        '<%= paths.src.all %>'
+      ]
+    },
 
-        watch: {
-            src: {
-                files: ['<%= paths.src.all %>'],
-                tasks: ['build']
-            }
-        },
+    watch: {
+      src: {
+        files: ['<%= paths.src.all %>'],
+        tasks: ['build']
+      }
+    },
 
-        concat: {
-            'default': {
-                src: ['<%= paths.src.default %>'],
-                dest: '<%= paths.dest.default %>'
-            },
-            'rsa': {
-                src: ['<%= paths.src.rsa %>'],
-                dest: '<%= paths.dest.rsa %>'
-            },
-            'kenya': {
-                src: ['<%= paths.src.kenya %>'],
-                dest: '<%= paths.dest.kenya %>'
-            }
-        },
+    concat: {
+      'default': {
+        src: ['<%= paths.src.default %>'],
+        dest: '<%= paths.dest.default %>'
+      },
+      'rsa': {
+        src: ['<%= paths.src.rsa %>'],
+        dest: '<%= paths.dest.rsa %>'
+      },
+      'kenya': {
+        src: ['<%= paths.src.kenya %>'],
+        dest: '<%= paths.dest.kenya %>'
+      }
+    },
 
-        mochaTest: {
-            options: {
-                reporter: 'spec'
-            },
-            test_default: {
-                src: ['<%= paths.test.default %>']
-            },
-            test_rsa: {
-                src: ['<%= paths.test.rsa %>']
-            },
-            test_kenya: {
-                src: ['<%= paths.test.kenya %>']
-            },
-            test_utils :{
-                src: ['<%= paths.test.utils %>']
-            }
-        }
-    });
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      test_default: {
+        src: ['<%= paths.test.default %>']
+      },
+      test_rsa: {
+        src: ['<%= paths.test.rsa %>']
+      },
+      test_kenya: {
+        src: ['<%= paths.test.kenya %>']
+      },
+      test_utils :{
+        src: ['<%= paths.test.utils %>']
+      }
+    }
+  });
 
-    grunt.registerTask('test', [
-        'jshint',
-        'build',
-        'mochaTest'
-    ]);
+  grunt.registerTask('test', [
+    'jshint',
+    'build',
+    'mochaTest'
+  ]);
 
-    grunt.registerTask('build', [
-        'concat'
-    ]);
+  grunt.registerTask('build', [
+    'concat'
+  ]);
 
-    grunt.registerTask('default', [
-        'build',
-        'test'
-    ]);
+  grunt.registerTask('default', [
+    'build',
+    'test'
+  ]);
 };
--- src/default.js
+++ src/default.js
@@ -1,29 +1,29 @@
 go.app = function() {
 
-    var vumigo = require('vumigo_v02');
-    var App = vumigo.App;
+  var vumigo = require('vumigo_v02');
+  var App = vumigo.App;
 
-    var GoMAMA = App.extend(function(self) {
-        App.call(self, 'states_start');
+  var GoMAMA = App.extend(function(self) {
+    App.call(self, 'states_start');
 
-        self.states.add('states_start', function (name, opts) {
-            return self.im.api_request('optout.status', {
-                address_type: "msisdn",
-                address_value: self.im
-            }).then(function (result) {
-                if(result.opted_out) {
-                    return self.states.create('opted_out');
-                } else if (self.im.config.default_language) {
-                    return self.states.create('default_language');
-                } else {
-                    return self.states.create('language_selection');
-                }
-            });
-        });
+    self.states.add('states_start', function (name, opts) {
+      return self.im.api_request('optout.status', {
+        address_type: "msisdn",
+        address_value: self.im
+      }).then(function (result) {
+        if(result.opted_out) {
+          return self.states.create('opted_out');
+        } else if (self.im.config.default_language) {
+          return self.states.create('default_language');
+        } else {
+          return self.states.create('language_selection');
+        }
+      });
     });
+  });
 
-    return {
-        GoMAMA: GoMAMA
-    };
+  return {
+    GoMAMA: GoMAMA
+  };
 
 }();
